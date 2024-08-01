a3661a5a27cdbeca319b9d7411e5395cf884e56b
luizgustavojunqueira <luizgustavossj@gmail.com>
2024-08-01 13:49:27 -0400

    fix: comment temporarily unused import

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import "./App.css";
 import Home from "./pages/Home";
 import Skills from "./pages/Skills";
 import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Projects from "./pages/Projects";
+// import Contact from "./pages/Contact";
+// import Projects from "./pages/Projects";
 
 function App() {
   return (
