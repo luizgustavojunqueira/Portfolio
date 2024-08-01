58189437ab2cb8f5016afbff84c01e5c9cb613a7
luizgustavojunqueira <luizgustavossj@gmail.com>
2024-08-01 13:08:07 -0400

    feat: SkillItem component

---
+++ src/components/SkillItem.tsx
@@ -0,0 +1,16 @@
+export default function SkillItem({
+  image,
+  name,
+}: {
+  image: string;
+  name: string;
+}) {
+  return (
+    <div className="item">
+      <div className="icon">
+        <img src={image} alt="" />
+      </div>
+      <p>{name}</p>
+    </div>
+  );
+}
--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -1,11 +1,19 @@
 import "../styles/skills.css";
-import javaScript from "../assets/icons8-javascript-48.png"
-import typesscript from "../assets/icons8-typescript-48.png"
-import python from "../assets/icons8-python-48.png"
-import c from "../assets/icons8-c-48.png"
-import cplusplus from "../assets/icons8-c-48 (1).png"
-import html from "../assets/icons8-html-48.png"
-import css from "../assets/icons8-css-48.png"
+import javaScript from "../assets/javascript_icon.svg";
+import typeScript from "../assets/typescript_icon.svg";
+import python from "../assets/python_icon.svg";
+import cplusplus from "../assets/c++_icon.svg";
+import html from "../assets/html_icon.svg";
+import css from "../assets/css_icon.svg";
+import c from "../assets/c_icon.svg";
+import react from "../assets/react_icon.svg";
+import node from "../assets/nodejs_icon.svg";
+import git from "../assets/git_icon.svg";
+import docker from "../assets/docker_icon.svg";
+import linux from "../assets/linux_icon.svg";
+import bash from "../assets/bash_icon.svg";
+import SkillItem from "../components/SkillItem";
+
 export default function Skills() {
   return (
     <section className="skills dark_gray_bg">
@@ -19,57 +27,26 @@ export default function Skills() {
         </section>
       </section>
       <div className="containerSkills">
-        <div className="item">
-          <div className="icon_js">
-             <img  src = {javaScript}  alt = ""/>
-             
-          </div>
-         <p>JavaScript</p>
+        <div className="skillsLine">
+          <SkillItem image={javaScript} name={"JavaScript"} />
+          <SkillItem image={typeScript} name={"TypeScript"} />
+          <SkillItem image={python} name={"Python"} />
+          <SkillItem image={c} name={"C"} />
+          <SkillItem image={cplusplus} name={"C++"} />
+          <SkillItem image={html} name={"HTML"} />
+          <SkillItem image={css} name={"CSS"} />
         </div>
-        <div className="item">
-        <div className="icon_js">
-             <img  src = {typesscript}  alt = ""/>
-             
-          </div>
-         <p>Typescript</p>
+        <div className="skillsLine">
+          <SkillItem image={node} name={"NodeJS"} />
+          <SkillItem image={react} name={"ReactJS"} />
+          <SkillItem image={git} name={"Git"} />
+          <SkillItem image={docker} name={"Docker"} />
         </div>
-        <div className="item">
-        <div className="icon_js">
-             <img  src = {python}  alt = ""/>
-             
-          </div>
-         <p>Python</p>
+        <div className="skillsLine">
+          <SkillItem image={linux} name={"Linux"} />
+          <SkillItem image={bash} name={"Bash"} />
         </div>
-        <div className="item">
-          <div className="icon_js">
-             <img  src = {c}  alt = ""/>
-             
-          </div>
-         <p>C</p>
-         </div>
-        <div className="item">
-        <div className="icon_js">
-             <img  src = {cplusplus}  alt = ""/>
-             
-          </div>
-         <p>C++</p>
-        </div>
-        <div className="item">  
-          <div className="icon_js">
-             <img  src = {html}  alt = ""/>
-             
-          </div>
-         <p>HTML</p>
-         </div>
-        <div className="item">
-        <div className="icon_js">
-             <img  src = {css}  alt = ""/>
-             
-          </div>
-         <p>CSS</p>
-        </div>
-        
-        </div>
+      </div>
     </section>
   );
 }
