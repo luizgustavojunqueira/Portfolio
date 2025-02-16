f0723946d3f1e9e240b9df9c2ae7e63c3b4525b1
Luiz Gustavo Junqueira <luizgustavossj@gmail.com>
2025-02-16 11:33:17 -0400

    feat: add arrow to go back to top

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
     }, [theme]);
     return (
         <>
-            <section id="app">
+            <main id="app">
                 <Header
                     theme={theme}
                     setTheme={setTheme}
@@ -32,12 +32,17 @@ function App() {
                 <Skills lang={lang} />
                 <Contact lang={lang} />
                 {/* <Projects /> */}
-                {/* <footer>
-                    <a href="#top">
-                        <img src={ArrowTopIcon} alt="Arrow to top" />
-                    </a>
-                </footer> */}
-            </section>
+            </main>
+            <a href="#app" className="arrow-top">
+                <svg
+                    className="icon"
+                    height="32px"
+                    viewBox="0 0 32 32"
+                    width="32px"
+                >
+                    <path d="M18.221,7.206l9.585,9.585c0.879,0.879,0.879,2.317,0,3.195l-0.8,0.801c-0.877,0.878-2.316,0.878-3.194,0  l-7.315-7.315l-7.315,7.315c-0.878,0.878-2.317,0.878-3.194,0l-0.8-0.801c-0.879-0.878-0.879-2.316,0-3.195l9.587-9.585  c0.471-0.472,1.103-0.682,1.723-0.647C17.115,6.524,17.748,6.734,18.221,7.206z" />
+                </svg>
+            </a>
         </>
     );
 }
