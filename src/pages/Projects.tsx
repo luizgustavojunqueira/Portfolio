07bdaf32be901663be570c0b247b5618f1370430
Luiz Gustavo Junqueira <luizgustavossj@gmail.com>
2025-02-16 17:01:56 -0400

    fix: remove placeholder projects

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -31,57 +31,6 @@ A personal portfolio made with ReactJS and CSS, where I talk a little about myse
                         repo="https://github.com/luizgustavojunqueira/portfolio"
                         link="https://portfolio-luizgustavojunqueiras-projects.vercel.app/"
                     />
-
-                    <ProjectItem
-                        lang={lang}
-                        name={lang === "pt" ? "Portfólio" : "Portfolio"}
-                        description={
-                            lang === "pt"
-                                ? `
-Um portfólio pessoal feito com ReactJS e CSS, onde falo um pouco sobre mim e mostro alguns dos meus projetos.
-`
-                                : `
-A personal portfolio made with ReactJS and CSS, where I talk a little about myself and show some of my projects.
-`
-                        }
-                        image="https://raw.githubusercontent.com/luizgustavojunqueira/Portfolio/refs/heads/main/Portfolio.png"
-                        repo="https://github.com/luizgustavojunqueira/portfolio"
-                        link="https://portfolio-luizgustavojunqueiras-projects.vercel.app/"
-                    />
-
-                    <ProjectItem
-                        lang={lang}
-                        name={lang === "pt" ? "Portfólio" : "Portfolio"}
-                        description={
-                            lang === "pt"
-                                ? `
-Um portfólio pessoal feito com ReactJS e CSS, onde falo um pouco sobre mim e mostro alguns dos meus projetos.
-`
-                                : `
-A personal portfolio made with ReactJS and CSS, where I talk a little about myself and show some of my projects.
-`
-                        }
-                        image="https://raw.githubusercontent.com/luizgustavojunqueira/Portfolio/refs/heads/main/Portfolio.png"
-                        repo="https://github.com/luizgustavojunqueira/portfolio"
-                        link="https://portfolio-luizgustavojunqueiras-projects.vercel.app/"
-                    />
-
-                    <ProjectItem
-                        lang={lang}
-                        name={lang === "pt" ? "Portfólio" : "Portfolio"}
-                        description={
-                            lang === "pt"
-                                ? `
-Um portfólio pessoal feito com ReactJS e CSS, onde falo um pouco sobre mim e mostro alguns dos meus projetos.
-`
-                                : `
-A personal portfolio made with ReactJS and CSS, where I talk a little about myself and show some of my projects.
-`
-                        }
-                        image="https://raw.githubusercontent.com/luizgustavojunqueira/Portfolio/refs/heads/main/Portfolio.png"
-                        repo="https://github.com/luizgustavojunqueira/portfolio"
-                        link="https://portfolio-luizgustavojunqueiras-projects.vercel.app/"
-                    />
                 </section>
             </section>
         </>
