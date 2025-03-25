dd2c4f49f32a2214e34c04aab78cda3e2b3879b7
Luiz Gustavo Junqueira <luizgustavossj@gmail.com>
2025-03-25 19:39:29 -0400

    feat: skills page redesign

--- src/components/SkillItem.tsx
+++ src/components/SkillItem.tsx
@@ -6,8 +6,8 @@ export default function SkillItem({
     name: string;
 }) {
     return (
-        <section className="flex-center column skill-item">
-            <img className="icon-img" src={image} alt="" />
+        <section className="w-25 md:w-40 flex flex-col items-center justify-center gap-2 bg-slate-100 dark:bg-darkgray p-2 text-sm rounded-xl text-current hover:bg-slate-300 dark:hover:bg-midgray">
+            <img className="w-20 h-20 object-contain" src={image} alt="" />
             <p>{name}</p>
         </section>
     );
