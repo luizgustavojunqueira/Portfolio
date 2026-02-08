2b3b4a4e79f96bab64ae4dbeff8e7fa2127ace67
Luiz Gustavo Junqueira <luizgustavossj@gmail.com>
2026-02-08 12:51:33 -0400

    fix: header color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -40,7 +40,7 @@ function Header({
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
-            className={`absolute top-0 w-6 h-6 fill-slate-900 transition-all duration-300 ${
+            className={`absolute top-0 w-6 h-6 fill-slate-200 transition-all duration-300 ${
               theme === "dark"
                 ? "opacity-100 scale-100 rotate-0"
                 : "opacity-0 scale-75 -rotate-45"
@@ -48,7 +48,7 @@ function Header({
           >
             <path
               d="M12 3V4M12 20V21M4 12H3M6.31412 6.31412L5.5 5.5M17.6859 6.31412L18.5 5.5M6.31412 17.69L5.5 18.5001M17.6859 17.69L18.5 18.5001M21 12H20M16 12C16 14.2091 14.2091 16 12 16C9.79086 16 8 14.2091 8 12C8 9.79086 9.79086 8 12 8C14.2091 8 16 9.79086 16 12Z"
-              stroke="black"
+              stroke="white"
               strokeWidth="2"
               strokeLinecap="round"
               strokeLinejoin="round"
