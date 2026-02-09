8e8132809454618915eaa0e2ab104625f1f74998
Luiz Gustavo Junqueira <luizgustavossj@gmail.com>
2026-02-09 17:25:22 -0400

    feat: update projects

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -33,25 +33,51 @@ export default function Projects({ lang }: { lang: "en" | "pt" }) {
                   ? "Um portfólio pessoal feito com ReactJS e CSS, onde falo um pouco sobre mim e mostro alguns dos meus projetos."
                   : "A personal portfolio made with ReactJS and CSS, where I talk a little about myself and show some of my projects.",
               image:
-                "https://raw.githubusercontent.com/luizgustavojunqueira/Portfolio/refs/heads/development/Images/Portfolio.png",
+                "https://raw.githubusercontent.com/luizgustavojunqueira/Portfolio/refs/heads/main/Images/Portfolio.png",
               tags: ["ReactJS", "CSS", "JavaScript"],
               githubUrl: "https://github.com/luizgustavojunqueira/portfolio",
               liveUrl: "https://portfolio.luizgustavojunqueira.com",
               isHighlight: true,
             },
             {
-              id: "blog",
-              title: "Blog",
+              id: "Blog",
+              title: lang === "pt" ? "Blog" : "Blog",
               description:
                 lang === "pt"
-                  ? "Meu Blog pessoal feito com Golang, Templ e TailwindCSS onde escrevo sobre tecnologia, programação e projetos pessoais."
-                  : "My personal Blog made with Golang, Templ and TailwindCSS where I write about technology, programming and personal projects.",
+                  ? "Meu blog pessoal feito com Hugo, onde escrevo sobre tecnologia, programação e outros assuntos relacionados."
+                  : "My personal blog made with Hugo, where I write about technology, programming and other related topics.",
               image:
-                "https://raw.githubusercontent.com/luizgustavojunqueira/Portfolio/refs/heads/development/Images/Blogo.png",
-              tags: ["Golang", "Templ", "TailwindCSS"],
-              githubUrl: "https://github.com/luizgustavojunqueira/Blogo",
+                "https://raw.githubusercontent.com/luizgustavojunqueira/Portfolio/refs/heads/main/Images/Blog.png",
+              tags: ["Hugo", "Markdown", "Static Site"],
+              githubUrl: "https://github.com/luizgustavojunqueira/MyBlog",
               liveUrl: "https://blog.luizgustavojunqueira.com",
               isHighlight: true,
+            },
+            {
+              id: "chatui",
+              title: "ChaTUI",
+              description:
+                lang === "pt"
+                  ? "Um sistema de chat no terminal, escrito em Golang, que suporta múltiplos usuários e salas de chat."
+                  : "A terminal chat system, written in Golang, that supports multiple users and chat rooms.",
+              image:
+                "https://raw.githubusercontent.com/luizgustavojunqueira/Portfolio/refs/heads/main/Images/Chatui.png",
+              tags: ["Golang", "Terminal", "Chat"],
+              githubUrl: "https://github.com/luizgustavojunqueira/ChaTUI",
+              isHighlight: false,
+            },
+            {
+              id: "blogo",
+              title: "Blogo",
+              description:
+                lang === "pt"
+                  ? "Um sistema de blog escrito com Golang, Templ e TailwindCSS, que utilizei como meu blog pessoal por um tempo"
+                  : "A blog system written with Golang, Templ and TailwindCSS, which I used as my personal blog for a while",
+              image:
+                "https://raw.githubusercontent.com/luizgustavojunqueira/Portfolio/refs/heads/main/Images/Blogo.png",
+              tags: ["Golang", "Templ", "TailwindCSS"],
+              githubUrl: "https://github.com/luizgustavojunqueira/Blogo",
+              isHighlight: false,
             },
             {
               id: "ecogis",
@@ -61,7 +87,7 @@ export default function Projects({ lang }: { lang: "en" | "pt" }) {
                   ? "Projeto de pesquisa e extensão no Laboratório de Engenharia de Software, da UFMS, para monitoramento e gestão dos biomas do Brasil, por meio da ferramenta WebGis"
                   : "Research and extension project at the Software Engineering Laboratory, UFMS, for monitoring and management of the biomes of Brazil, through the WebGis tool",
               image:
-                "https://github.com/luizgustavojunqueira/Portfolio/blob/development/Images/ecogis.png?raw=true",
+                "https://github.com/luizgustavojunqueira/Portfolio/blob/main/Images/ecogis.png?raw=true",
               tags: ["WebGIS", "Research"],
               isHighlight: false,
             },
@@ -73,7 +99,7 @@ export default function Projects({ lang }: { lang: "en" | "pt" }) {
                   ? "Projeto de iniciação científica no LEDES (UFMS). Uma aplicação web para gerenciamento de inventários urbanos qualitativos e quantitativos."
                   : "Scientific initiation project at LEDES (UFMS). A web application for managing qualitative and quantitative urban inventories.",
               image:
-                "https://github.com/luizgustavojunqueira/Portfolio/blob/development/Images/ArboMais.png?raw=true",
+                "https://github.com/luizgustavojunqueira/Portfolio/blob/main/Images/ArboMais.png?raw=true",
               tags: ["Web App", "IC", "LEDES"],
               isHighlight: false,
             },
